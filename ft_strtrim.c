0394922e3d4b6912e5d60d3231da0953098f01e4
user42 <williams.ozserttas05@gmail.com>
2020-11-26 11:08:04 +0100

    Correction of ft_strtrim.c function

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 14:50:46 by user42            #+#    #+#             */
-/*   Updated: 2020/11/26 02:18:35 by user42           ###   ########.fr       */
+/*   Updated: 2020/11/26 11:06:55 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,28 +26,52 @@ static int		ft_is_charset(char c, const char *charset)
 	return (0);
 }
 
+static char		*get_trimed_str(char const *s1, char const *set, size_t end)
+{
+	char	*new_str;
+	size_t	start;
+	size_t	copy;
+
+	start = 0;
+	while (s1[start] != '\0' && ft_is_charset(s1[start], set) == 1)
+		start++;
+	if (!(new_str = (char*)malloc(sizeof(char) * ((end - start) + 2))))
+		return (NULL);
+	copy = 0;
+	while (start < end + 1)
+	{
+		new_str[copy] = s1[start];
+		copy++;
+		start++;
+	}
+	new_str[copy] = '\0';
+	return (new_str);
+}
+
 char			*ft_strtrim(char const *s1, char const *set)
 {
 	char	*new_str;
-	size_t	size;
+	size_t	end;
 	size_t	i;
-	size_t	copy;
 
-	size = 0;
 	if (s1 == NULL)
 		return (NULL);
 	i = 0;
-	while (ft_is_charset(s1[i], set) == 1)
+	while (s1[i] != '\0' && ft_is_charset(s1[i], set) == 1)
 		i++;
-	while (s1[size] != '\0')
-		size++;
-	while (size > 0 && ft_is_charset(s1[size], set) == 1)
-		size--;
-	if (!(new_str = (char*)malloc(sizeof(char) * ((i + size) + 1))))
-		return (NULL);
-	new_str[i + size] = '\0';
-	copy = 0;
-	while (i < size + 1)
-		new_str[copy++] = s1[i++];
+	if (s1[0] == '\0' || s1[i] == '\0')
+	{
+		if (!(new_str = (char*)malloc(sizeof(char) * 1)))
+			return (NULL);
+		new_str[0] = '\0';
+		return (new_str);
+	}
+	else
+	{
+		end = ft_strlen(s1) - 1;
+		while (end > 0 && ft_is_charset(s1[end], set) == 1)
+			end--;
+		new_str = get_trimed_str(s1, set, end);
+	}
 	return (new_str);
 }
