7e5ed21dc3cde7d62220e7a8438013693dc36141
user42 <williams.ozserttas05@gmail.com>
2020-11-26 12:39:39 +0100

    Correction of ft_atoi.c for over long value

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,49 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 19:02:03 by user42            #+#    #+#             */
-/*   Updated: 2020/11/25 19:19:52 by user42           ###   ########.fr       */
+/*   Updated: 2020/11/26 12:38:39 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft.h"
+
+static int	is_over_long(const char *str, int sign, int i)
+{
+	if (sign == 1 && str[i] == '9' && str[i + 1] >= '2' && str[i + 2] >= '2' &&
+		str[i + 3] >= '3' && str[i + 4] >= '3' && str[i + 5] >= '7' &&
+		str[i + 6] >= '2' && str[i + 7] >= '0' && str[i + 8] >= '3' &&
+		str[i + 9] >= '6' && str[i + 10] >= '8' && str[i + 11] >= '5' &&
+		str[i + 12] >= '4' && str[i + 13] >= '7' && str[i + 14] >= '7' &&
+		str[i + 15] >= '5' && str[i + 16] >= '8' && str[i + 17] >= '0' &&
+		str[i + 18] > '7')
+		return (-1);
+	else if (sign == -1 && str[i] == '9' && str[i + 1] >= '2' &&
+		str[i + 2] >= '2' && str[i + 3] >= '3' && str[i + 4] >= '3' &&
+		str[i + 5] >= '7' && str[i + 6] >= '2' && str[i + 7] >= '0' &&
+		str[i + 8] >= '3' && str[i + 9] >= '6' && str[i + 10] >= '8' &&
+		str[i + 11] >= '5' && str[i + 12] >= '4' && str[i + 13] >= '7' &&
+		str[i + 14] >= '7' && str[i + 15] >= '5' && str[i + 16] >= '8' &&
+		str[i + 17] >= '0' && str[i + 18] > '7')
+		return (0);
+	return (1);
+}
+
+static int	check_errors(const char *str, int sign, int lenght)
+{
+	while (*str == '0')
+		str++;
+	while (str[lenght] >= '0' && str[lenght] <= '9')
+		lenght++;
+	if (sign == 1 && lenght > 19)
+		return (-1);
+	else if (sign == -1 && lenght > 19)
+		return (0);
+	else if (is_over_long(str, sign, 0) == -1)
+		return (-1);
+	else if (is_over_long(str, sign, 0) == 0)
+		return (0);
+	return (1);
+}
 
 int			ft_atoi(const char *nptr)
 {
@@ -29,6 +67,10 @@ int			ft_atoi(const char *nptr)
 			sign *= -1;
 		i++;
 	}
+	if (check_errors(nptr + i, sign, 0) == 0)
+		return (0);
+	else if (check_errors(nptr + i, sign, 0) == -1)
+		return (-1);
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
 		res = res * 10 + (nptr[i] - '0');
